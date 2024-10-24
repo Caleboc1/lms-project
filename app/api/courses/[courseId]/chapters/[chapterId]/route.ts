6af2be80a375e20658d72b253a6af15f2d8caacc
“Caleb” <cakpomughe@gmail.com>
2024-10-24 23:33:10 +0100

    chapter video and access form , mux integration for videos

--- app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -1,7 +1,9 @@
+import Mux from "@mux/mux-node";
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
-
 import { db } from "@/lib/db";
+
+const mux = new Mux();
 
 export async function PATCH(
     req: Request,
@@ -9,10 +11,10 @@ export async function PATCH(
 ) {
     try {
         const { userId } = auth();
-        const {isPublished, ...values} = await req.json();
+        const { isPublished, ...values } = await req.json();
 
-        if(!userId){
-            return new NextResponse("Unauthorized", {status: 401});
+        if (!userId) {
+            return new NextResponse("Unauthorized", { status: 401 });
         }
 
         const ownCourse = await db.course.findUnique({
@@ -22,8 +24,8 @@ export async function PATCH(
             }
         });
 
-        if(!ownCourse){
-            return new NextResponse("Unauthorized", {status: 401});
+        if (!ownCourse) {
+            return new NextResponse("Unauthorized", { status: 401 });
         }
 
         const chapter = await db.chapter.update({
@@ -36,12 +38,44 @@ export async function PATCH(
             }
         });
 
-        //todo: handle video upload 
+        if (values.videoUrl) {
+            const existingMuxData = await db.muxData.findFirst({
+                where: {
+                    chapterId: params.chapterId,
+                }
+            });
+
+            if (existingMuxData) {
+
+                await mux.video.assets.delete(existingMuxData.assetId);
+                await db.muxData.delete({
+                    where: {
+                        id: existingMuxData.id,
+                    }
+                });
+            }
+
+            const asset = await mux.video.assets.create({
+                input: [{
+                    url: values.videoUrl, // Ensure this is a valid Mux-compatible URL
+                }],
+                playback_policy: ["public"], // Set playback policy here
+            });
+
+
+            // Store new Mux data in the database
+            await db.muxData.create({
+                data: {
+                    chapterId: params.chapterId,
+                    assetId: asset.id,
+                    playbackId: asset.playback_ids?.[0]?.id,
+                }
+            });
+        }
 
         return NextResponse.json(chapter);
     } catch (error) {
-        console.log("[COURSES_CHAPTER-ID", error)
+        console.error("[COURSES_CHAPTER-ID]", error);
         return new NextResponse("Internal Error", { status: 500 });
-
     }
 }
