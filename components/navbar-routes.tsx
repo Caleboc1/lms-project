7ed2bfbad21ef485e7de0a1fe4911c58d523ba9f
“Caleb” <cakpomughe@gmail.com>
2024-10-30 15:53:02 +0100

    2nd commit

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -6,6 +6,7 @@ import { LogOut } from "lucide-react";
 import { Button } from "./ui/button";
 
 import Link from "next/link";
+import { SearchInput } from "./search-input";
 // import DarkMode from "./darkmode/darkmode"
 
 export const NavbarRoutes = () => {
@@ -13,26 +14,34 @@ export const NavbarRoutes = () => {
 
     const isTeacherPage = pathname?.startsWith("/teacher");
     const isPlayerPage = pathname?.includes("/chapter");
+    const isSearchPage = pathname === "/search"
 
 
-    return(
-        <div className="flex gap-x-2 ml-auto">
-            {isTeacherPage || isPlayerPage ? (
-                <Link href="/">
-                <Button size="sm" variant="ghost">
-                  <LogOut className="h-4 w-4 mr-2 "/>
-                  Exit
-                </Button>
-                </Link>
-            ) : (
-                <Link href="/teacher/courses">
-                <Button size="sm" variant="ghost">
-                    Teacher mode
-                </Button>
-                </Link>
+    return (
+        <>
+            {isSearchPage && (
+                <div className="hidden md:block">
+                    <SearchInput />
+                </div>
             )}
-         <UserButton afterSignOutUrl="/"/>
-         {/* <DarkMode/> */}
-        </div>
+            <div className="flex gap-x-2 ml-auto">
+                {isTeacherPage || isPlayerPage ? (
+                    <Link href="/">
+                        <Button size="sm" variant="ghost">
+                            <LogOut className="h-4 w-4 mr-2 " />
+                            Exit
+                        </Button>
+                    </Link>
+                ) : (
+                    <Link href="/teacher/courses">
+                        <Button size="sm" variant="ghost">
+                            Teacher mode
+                        </Button>
+                    </Link>
+                )}
+                <UserButton afterSignOutUrl="/" />
+                {/* <DarkMode/> */}
+            </div>
+        </>
     )
 }
