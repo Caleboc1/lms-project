f461742117ce3e45704d30b17f1ebf8049ee7cde
“Caleb” <cakpomughe@gmail.com>
2024-10-30 13:35:49 +0100

    worked on rendering the list of courses created in the course route, sign-in buttion on the navbar when user isnt logged in

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,14 +1,31 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { DataTable } from "./_components/data-table";
+import { columns } from "./_components/columns";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/dist/server/api-utils";
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
 
-const CoursesPage = () => {
+
+const CoursesPage = async () => {
+    const {userId} = auth();
+
+    if (!userId) {
+        return NextResponse.redirect("/");
+    }
+
+    const courses = await db.course.findMany({
+        where: {
+            userId,
+        },
+        orderBy: {
+            createdAt: "desc",
+        },
+    });  
     return (  
         <div className="p-6">
-            <Link href="/teacher/create">
-           <Button >
-            New Course
-           </Button>
-           </Link>
+                <DataTable columns={columns} data={courses} />
         </div>
     );
 }
--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,12 +1,17 @@
-import { NavbarRoutes } from "@/components/navbar-routes"
-import { MobileSidebar } from "./mobile-sidebar"
+import { NavbarRoutes } from "@/components/navbar-routes";
+import { MobileSidebar } from "./mobile-sidebar";
+import { SignInButton, useUser } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
 
 export const Navbar = () => {
+    const { userId} = auth(); // Check if user is signed in
+
     return (
- <div className="p-4 border-b h-full flex items-center
- bg-white shadow-sm dark:bg-black">
-<MobileSidebar />
-<NavbarRoutes />
- </div>
-    )
-}
+        <div className="p-4 border-b h-full flex items-center bg-white shadow-sm dark:bg-black">
+            <MobileSidebar />
+            <NavbarRoutes />
+            {/* Show SignInButton only if user is not signed in */}
+            {!userId && <SignInButton />}
+        </div>
+    );
+};
